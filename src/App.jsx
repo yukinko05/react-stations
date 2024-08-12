1e41d9508b216ebe00a143b3d1e8bf4dcf2bff39
yukinko05 <grey.sora.35@gmail.com>
2024-08-12 10:42:25 +0900

    Station3-5を完了

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,27 @@
 // DO NOT DELETE
 
+import { useState } from 'react'
 import './App.css'
 
 /**
  * @type {() => JSX.Element}
  */
 export const App = () => {
+  const [dogUrl, setDogUrl] = useState(
+    'https://images.dog.ceo/breeds/sheepdog-shetland/n02105855_7560.jpg',
+  )
+
+  const handleClick = () => {
+    console.log('ボタンが押されました！')
+    setDogUrl('https://images.dog.ceo/breeds/malamute/n02110063_10751.jpg')
+  }
+
   return (
     <div>
       <header>Dogアプリ</header>
       <p>犬の画像を表示するサイトです</p>
-      <img
-        src="https://images.dog.ceo/breeds/sheepdog-shetland/n02105855_7560.jpg"
-        alt="犬の画像"
-      />
+      <img src={dogUrl} alt="犬の画像" />
+      <button onClick={handleClick}>更新</button>
     </div>
   )
 }
