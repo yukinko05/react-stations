0058bb46421a9f99522e7f330b1c3f1106e7730f
yukinko05 <grey.sora.35@gmail.com>
2024-08-12 10:24:25 +0900

    Station2を完了

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,11 @@ export const App = () => {
   return (
     <div>
       <header>Dogアプリ</header>
+      <p>犬の画像を表示するサイトです</p>
+      <img
+        src="https://images.dog.ceo/breeds/sheepdog-shetland/n02105855_7560.jpg"
+        alt="犬の画像"
+      />
     </div>
   )
 }
