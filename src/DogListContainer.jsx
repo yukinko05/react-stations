c793bfd3b1fc0e58c82fbd3730ed628670532812
yukinko05 <grey.sora.35@gmail.com>
2024-08-13 10:01:07 +0900

    Station9を完了

--- src/DogListContainer.jsx
+++ src/DogListContainer.jsx
@@ -1,6 +1,20 @@
 // @ts-check
 
+import { useEffect, useState } from 'react'
+
 export const DogListContainer = () => {
+  const [breeds, setBreeds] = useState('')
+
+  useEffect(() => {
+    fetch('https://dog.ceo/api/breeds/list/all')
+      .then(res => res.json())
+      .then(result => {
+        setBreeds(result.message)
+      })
+      .catch(error => {
+        console.error('データ取得に失敗しました', error)
+      })
+  })
   return <></>
 }
 
